ada6a9a68866f3a1e79e4f7714fc47e93bf431d4
Cajol1e <zhl>
2025-11-28 10:40:27 +0800

    second

--- source/conf.py
+++ source/conf.py
@@ -1,32 +1,47 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'soul-docs'
-copyright = '2025, Tecl'
-author = 'Tecl'
+copyright = '2025, Soul'
+author = 'Soul'
 release = 'v0.0.1'
+    
+extensions = [
+    'myst_parser',
+    "sphinxcontrib.mermaid",
+    "sphinx_copybutton",
+]
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
 
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
+myst_enable_extensions = [
+    "tasklist",
+    "deflist",
+    "dollarmath",
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+language = 'zh_CN'
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'analytics_anonymize_ip': False,
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+}
+
+html_logo = "./_static/logo.png"
 html_static_path = ['_static']
+html_js_files = [
+    'my_custom.js',
+]
